725b1291a5e255ec7190fcc63aba7375916763d4
Martin Slater <mslater@darkstorm.com.au>
2018-06-07 16:38:48 +0300

    first plausible implementation

--- scienspeak.py
+++ scienspeak.py
@@ -6,7 +6,11 @@
 # Created : Thursday, 07 June 2018 02:56:57 PM
 #-----------------------------------------------------------------------------
 """
-Scienspeak command line tool.
+Tool for generating a plausible way of pronouncing scientific names. The way will
+probably be wrong but as many point out most pronunciations are anyway, but at
+least you will have some conistency to your wrongness. Given a word (or series of words)
+on the command line it will output them punctuated into individual syllables with the
+syllable that should receive the stress in capitals.
 """
 
 #-----------------------------------------------------------------------------
@@ -14,21 +18,58 @@ Scienspeak command line tool.
 #-----------------------------------------------------------------------------
 from __future__ import absolute_import, print_function
 import argparse
-import scyli
+import sys
+import sylli
 
 #-----------------------------------------------------------------------------
 # Class
 #-----------------------------------------------------------------------------
-
 class Scienspeak(object):
     """ Scienspeak """
-    
+
     def __init__(self, args):
         """ Constructor """
-        pass
+        # convert 'ae' digraph to an 'e' otherwise the syllabliser will get confused.
+        # in latin it is a single vowel.
+        words = args.words[0].replace('ae', 'e')
+
+        self.words = [ word.strip().lower() for word in words.split(' ') ]
 
     def run(self):
-        pass
+        syl = sylli.SylModule()
+        for word in self.words:
+            # split into individual syllables
+            syllables = syl.syllabify(word).split('.')
+            final = []
+
+            #-----------------------------------------------------------------------------
+            # Stressing rules for syllables
+            #-----------------------------------------------------------------------------
+            # == 1 -> Stress on it
+            # == 2 -> Stress on first syllable, sound s short vowel if followed by 2 consonants
+            # >= 2 -> IF second to last syllable is followed by 2 consonants
+            #         OR the vowel is long (?)
+            #         THEN stress second to last syllable
+            #         ELSE stress third to last syllable
+            #-----------------------------------------------------------------------------
+            if len(syllables) == 1:
+                final.append(syllables[0].upper())
+            elif len(syllables) == 2:
+                final.append(syllables[0].upper())
+                final.append(syllables[1])
+            else:
+                #TODO: how to tell if it is a long vowel with a macron that is used to indicate it
+                #      we just use 2nd to last for now
+                num_syllables = len(syllables)
+                for idx in range(num_syllables):
+                    cur = syllables[idx]
+                    if idx == (num_syllables-2):
+                        final.append(cur.upper())
+                    else:
+                        final.append(cur)
+
+            sys.stdout.write('.'.join(final) + ' ')
+
 
 #-----------------------------------------------------------------------------
 # Main
@@ -37,9 +78,7 @@ class Scienspeak(object):
 def main():
     """ Main script entry point """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-a', '--arg',
-                        help='A boolean arg',
-                        dest='barg', action='store_true')
+    parser.add_argument('words', nargs='+')
     args = parser.parse_args()
     Scienspeak(args).run()
 
